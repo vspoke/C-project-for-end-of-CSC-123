11c5d0843155e009e330a4d27b3c9124c1dfbd84
Vincent Sambor <122117324+vspoke@users.noreply.github.com>
2023-01-08 13:03:02 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,20 @@ using namespace std;
 #include <vector>
 
 int main() {
+//defined class that is used to intialize game and play game
   controller build;
+//string that stops the game initialized to y to start game
   string x = "y"; 
+/*
+While keeps game running if the string isn't intialized to the 
+value to break out and if actually plays the game if the string 
+is initalized to start game value
+*/
   while(x!="n"){
     if(x=="y"){
       cout << "How many players:";
       getline(cin, x);
+//main game exists as class
       build.initializeGame(stoi(x));
       build.playGame();
     }
